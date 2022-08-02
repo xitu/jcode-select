34ee406e32136d52bc7e35ce61c3248bf72cfa99
akira-cn <19511344@qq.com>
2022-08-02 17:54:12 +0800

    update doc

--- jcoderc.js
+++ jcoderc.js
@@ -5,18 +5,41 @@ export default {
     {
       type: 'folder',
       folded: false,
-      name: '使用技巧',
+      name: '基本用法',
       collection: [
-        {name: '第一章', author: 'akira-cn', id: '7126827014727663630', default: true},
-        {name: '第二章', author: 'akira-cn', id: '7126838959774105637'},
+        {name: '码上掘金简介', author: 'akira-cn', id: '7127211087010725902', default: true},
+        {name: '使用码上掘金', author: 'akira-cn', id: '7127212028904767495'},
+        {name: 'JS 和 TS', author: 'akira-cn', id: '7127212396124307493'},
+        {name: '前端框架 React', author: 'akira-cn', id: '7127212565695823880'},
+        {name: '前端框架 Vue', author: 'akira-cn', id: '7127212771376103431'},
+        {name: '系统设置', author: 'akira-cn', id: '7127212875076075550'},
+      ],
+    }, {
+      type: 'folder',
+      folded: false,
+      name: '高级用法',
+      collection: [
+        {name: 'CSS工具', author: 'akira-cn', id: '7127213170216665119'},
+        {name: 'CDN模块', author: 'akira-cn', id: '7127213387217371143'},
+        {name: '扩展工具', author: 'akira-cn', id: '7127213522273959943'},
+        {name: '自定义语言', author: 'akira-cn', id: '7127213667807920136'},
+        {name: '服务端能力', author: 'akira-cn', id: '7127213884393390117'},
+        {name: '创建合辑', author: 'akira-cn', id: '7127214014160977927'},
+        {name: '社区生态', author: 'akira-cn', id: '7127214186760765476'},
+      ],
+    }, {
+      type: 'folder',
+      folded: false,
+      name: 'FAQ',
+      collection: [
+        {name: '为什么我的代码报错', author: 'akira-cn', id: '7127214375579942948'},
       ],
     }, {
       type: 'folder',
       folded: true,
-      name: '示例代码',
+      name: '精彩代码',
       collection: [
-        {name: '3d fox', author: 'akira-cn', id: '7084148729669746721'},
-        {name: 'Pass wave', author: 'akira-cn', id: '7084111941693603877'},
+        {name: '3d fox', author: 'akira-cn', id: '7111233570496053255'},
       ],
     },
   ],
